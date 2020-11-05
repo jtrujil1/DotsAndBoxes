90f8ae77effd08552986c6de6905bb024d29d106
jtrujil1 <jtrujil1@trinity.edu>
2020-11-05 16:33:09 -0600

    Representation milestone

--- KnotsAndWhatNots.hs
+++ KnotsAndWhatNots.hs
@@ -6,11 +6,12 @@ import Data.Maybe
 import Debug.Trace
 
 --Data Types--
-data Board = EmptyBoard | FullBoard deriving Show
-data Line = (Dot, Dot) deriving Show
+type Board = [Line] deriving Show
+type Line = (Dot, Dot) deriving Show
 data Dots = (Int, Int)
-data Box = (Line, Line, Line, Line) deriving Show
-data Player = Player1 Int [Box] | Player2 Int [Box] deriving Show
+type Box = Dot deriving Show
+data Player = Player1 | Player2
+data PlayerScores = (P1 (Player, Int, [Box]), P2 (Player, Int, [Box])) deriving Show
 
 --Possible Functions
 --createBoard
@@ -20,42 +21,52 @@ data Player = Player1 Int [Box] | Player2 Int [Box] deriving Show
 --ValidDot
 --remove (remove dots/lines that have already been played)
 --
+
+--check if board is full
+checkBoard :: Bool
+checkBoard = undefined 
+
 --creates original board at start //start with 5 x 5
 createBoard :: Board
 createBoard = undefined
 
 --Update board after each move[line] is made
-updateBoard :: [Dots] -> [Line] -> Board
+updateBoard :: [Line] -> Board
 updateBoard = undefined
 
 --stores list of possible dots to be made
 allDots :: [Dot]
 allDots dots = [(x,y)| x <- [0..5], y <- [0..5]]
 
---lists of current dots on board at each move
-currentDots :: [Dot]
-currentDots = undefined
-
 --lists of current lines on board after each move
 currentLines :: [Line]
 curentLines = undefined
-
---checks if box is even possible [formula for lines to make box]
-checkLines :: [Line] -> Box
-checkLines = undefined
 
 --remove dots/lines from lists
 remove :: Eq a => a -> [a] -> [a]
 remove = undefined
 
 --checks to see if line is valid
-checkLine :: Dot -> Dot ->Bool
-checkLine = undefined
+validLine :: Dot -> Dot -> Bool
+validLine = undefined
 
 --checks to see if box has been made already
-validBox :: Box -> Box
+validBox :: Box -> Bool
 validBox = undefined
 
 --checks highest number of box to declare winner
-playerWin :: [Box] -> [Box] -> Bool
+playerWin :: PlayerScores -> Player
 playerWin = undefined
+
+--takes in a box and updates the player scores
+updateScore :: Player -> Box -> PlayerScores
+updateScore = undefined
+
+--check if line is valid
+--check if new box is formed
+--update the board
+--give back the player who's next
+makePlay :: Player -> Line -> Player
+makePlay = undefined
+
+
